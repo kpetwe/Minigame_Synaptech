36c1b38ff00584239d4a18913ffff283f556f64f
trnanhkit12 <95549016+trnanhkit12@users.noreply.github.com>
2022-02-05 14:00:17 -0800

    Add files via upload

--- Scripts/BasicMovement.cs
+++ Scripts/BasicMovement.cs
@@ -4,18 +4,54 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-    [SerializeField] float speed = 0.10f;
-
+    public float speed = 0.10f;
+    PlayerMovement PlayerMovement;
+    public GameObject Player;
+    public Animator animator;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        PlayerMovement = Player.GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.time > 90)
+        {
+            Debug.Log("You've won");
+        }
+        if (Input.GetKey("left"))
+        {
+            speed -= 0.01f;
+            animator.SetFloat("speed", speed);
+        }
+        if (Input.GetKey("right"))
+        {
+            speed += 0.01f;
+            animator.SetFloat("speed", speed);
+        }
+        // if stress -> increase spd
+        // if calm -> decrease spd
+        // if spd of villain > spd of character
         Vector3 horizontal = new Vector3(speed, 0, 0);
+            // close distance: villain's spd is positive -> run animation
+        // if spd of villain < spd of character
+            // villain's spd is negative -> walk animation
+            // can only reaches at the end of screen
+        // if spd of villain == spd of character
+            // villain's spd == 0 walk animation
         transform.position = transform.position + horizontal * Time.deltaTime;
+        
+        // Print winning message 
+    }
+
+    //On collision
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Debug.Log("You lose");
+        }
     }
 }
